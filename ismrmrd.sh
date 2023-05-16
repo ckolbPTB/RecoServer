b960a3970559bd3180fc3bc945960c2102d2bfb9
Christoph Kolbitsch <ptbgenreg@gmail.com>
2023-05-16 16:11:57 +0200

    sirf added

--- ismrmrd.sh
+++ ismrmrd.sh
@@ -9,7 +9,7 @@ ${APT_GET_INSTALL} g++ cmake
 ${APT_GET_INSTALL} libboost-all-dev xsdcxx libxerces-c-dev \
     libhdf5-serial-dev h5utils hdf5-tools libtinyxml-dev libxml2-dev libxslt1-dev
 
-git clone https://github.com/ismrmrd/ismrmrd.git --depth 1
+git clone https://github.com/ismrmrd/ismrmrd.git --depth 1 --branch ${ISMRMRD_TAG}
 
 cd ismrmrd
 mkdir build
@@ -18,7 +18,7 @@ cmake ..
 make -j 20
 make install
 
-git clone https://github.com/ismrmrd/siemens_to_ismrmrd.git --depth 1
+git clone https://github.com/ismrmrd/siemens_to_ismrmrd.git --depth 1 --branch ${SIEMENS_TO_ISMRMRD_TAG}
 cd siemens_to_ismrmrd
 mkdir build
 cd build
--- ubuntu.sh
+++ ubuntu.sh
@@ -12,8 +12,17 @@ locale-gen en_GB.UTF-8
 export LANG=en_GB.UTF-8
 export LANGUAGE=en_GB:en
 
+# ensure certificates are up to date
+${APT_GET_INSTALL} --reinstall ca-certificates
+
 # base utilities
-${APT_GET_INSTALL} build-essential wget swig libomp-dev screen locate
+${APT_GET_INSTALL} build-essential python3-dev wget swig libomp-dev screen locate pkg-config curl
+
+# CMake
+curl -o cmake.tgz -L https://github.com/Kitware/CMake/releases/download/v3.25.1/cmake-3.25.1-linux-x86_64.tar.gz
+tar xzf cmake.tgz && rm cmake.tgz
+ln -s cmake-*x86_64 cmake || true
+export PATH="$PWD/cmake/bin:$PATH"
 
 # git
 ${APT_GET_INSTALL} git
