21769f46a23136be404de2317cf46d9ec49ec176
Christoph Kolbitsch <christoph.kolbitsch@ptb.de>
2023-05-18 21:17:37 +0200

    skcuda and numpy conflict fixed

--- ptbrecon.sh
+++ ptbrecon.sh
@@ -5,6 +5,7 @@ conda update -n base -c defaults conda
 conda env create --file ptbrecon_environment.yml
 source /opt/conda/bin/activate PTBEnv
 conda install ipykernel
-conda install -y pytorch cudatoolkit -c pytorch
-pip install pycuda scikit-cuda
-pip install torchkbnufft
+conda install -y pytorch=2.0 cudatoolkit -c pytorch
+conda install pycuda
+pip install git+https://github.com/lebedov/scikit-cuda.git@2591b56
+pip install torchkbnufft=1.4
