46579e3efd7e44ed22025c77cdf6049cb13b7e9a
Christoph Kolbitsch <christoph.kolbitsch@ptb.de>
2023-11-22 11:39:25 +0100

    nvidia keys update

--- ubuntu.sh
+++ ubuntu.sh
@@ -18,6 +18,10 @@ ${APT_GET_INSTALL} --reinstall ca-certificates
 # base utilities
 ${APT_GET_INSTALL} build-essential python3-dev wget swig libomp-dev screen locate pkg-config curl
 
+# ensure nvidia signing keys are up to date
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.0-1_all.deb
+dpkg -i cuda-keyring_1.0-1_all.deb
+
 # CMake
 curl -o cmake.tgz -L https://github.com/Kitware/CMake/releases/download/v3.25.1/cmake-3.25.1-linux-x86_64.tar.gz
 tar xzf cmake.tgz && rm cmake.tgz
